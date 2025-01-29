3d539c7c90beff0cd98569f20dc5d639603434ef
LuiselbotYT <luisdorabae@gmail.com>
2025-01-29 08:53:14 -0800

    neovim: added vim-plug auto install

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,14 @@ vim.cmd("syntax enable")
 
 ---- plugins
 
+local data_dir = vim.fn.stdpath('data')
+if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
+    vim.cmd('silent !curl -fLo ' ..
+	data_dir ..
+	'/site/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
+    vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
+end
+
 local vim = vim
 local Plug = vim.fn['plug#']
 
